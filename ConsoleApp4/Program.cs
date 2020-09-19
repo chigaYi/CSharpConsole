cdfa25af9aa3a28ba6254ca4a27aaaee1ea6b97a
chigajames <61407295+chigajames@users.noreply.github.com>
2020-09-19 18:33:00 +0800

    chiga change

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -15,6 +15,8 @@ namespace ConsoleApp4
             Console.WriteLine("別人修改了一些東西");
             //lolmuta加入新功能
             Console.WriteLine("lolmuta加入新功能");
+            //chiga fix something
+            Console.WriteLine("chiga fix something");
         }
     }
 }
